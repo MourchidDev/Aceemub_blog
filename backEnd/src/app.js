1dc660249142341c6d7a15f2c1b4567515e97b31
Mourchid Assani <mourchidhssn@gmail.com>
2026-05-07 11:55:05 +0200

    retour sur la version 6 de prisma

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -2,8 +2,17 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import { rateLimit } from "express-rate-limit";
+import prisma from "./config/prisma.js"
 const app = express();
 
+
+
+async function test() {
+    await prisma.$connect();
+    console.log("✅ Connexion à la base de données réussie");
+}
+
+test();
 app.get("/api/health", (_req, res) => res.json({ status: "ok" }));
 
 export default app;
