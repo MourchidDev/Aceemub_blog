785bc837aeee673dab83250c64fb46d683710e6f
Mourchid Assani <mourchidhssn@gmail.com>
2026-05-09 11:31:03 +0200

    methodes de get update delete article

--- backEnd/src/modules/articles/articleController.js
+++ backEnd/src/modules/articles/articleController.js
@@ -1,5 +1,13 @@
 import createArticle from "./articleService.js";
-
+import getArticles from "./articleService.js";
+import getArticleById from "./articleService.js";
+import getArticlesByCategory from "./articleService.js";
+import getAllArticles from "./articleService.js";
+import getArticlesByAuthor from "./articleService.js";
+import updateArticle from "./articleService.js";
+import deleteArticle from "./articleService.js";
+import publishArticle  from "./articleService.js";
+import archiveArticle  from "./articleService.js";
 const createArticleController = async (req, res, next) => {
     try {
         const user = req.user ?? {id: "1d257d43-47ec-4b9e-8f47-62681347fa65"};
--- backEnd/src/modules/articles/articleService.js
+++ backEnd/src/modules/articles/articleService.js
@@ -1,3 +1,4 @@
+import { cpSync } from "node:fs";
 import prisma from "../../config/prisma.js";
 import articleSchema from "../../validators/articleValidator.js";
 
@@ -26,4 +27,108 @@ const createArticle = async (data, user) => {
     return article;
 }
 
-export default createArticle;
+const getArticles = async () => {
+    const articles = await prisma.article.findMany({
+        where:{status: "PUBLISHED"},
+        include:{
+            author:true,
+            category:true
+        }
+    });
+    return articles;
+}
+
+const getArticleById = async (id) => {
+    const article = await prisma.article.findUnique({
+        where:{id},
+        include:{
+            author:true,
+            category:true
+        }
+    });
+    return article;
+}
+
+const getArticlesByCategory = async (categoryId) => {
+    const articles = await prisma.article.findMany({
+        where:{
+            categoryId,
+            status: "PUBLISHED"
+        },
+        include:{
+            author:true,
+            category:true
+        }
+    });
+    return articles;
+}
+
+const getAllArticles = async() => {
+    const articles = await prisma.article.findMany({
+       include: {
+        author: true,
+        category: true
+       } 
+    })
+}
+
+const getArticlesByAuthor = async(authorId) => {
+    const articles = await prisma.article.findMany({
+        where:{
+            authorId,
+            status: "PUBLISHED"
+        },
+        include:{
+            author:true,
+            category:true
+        }
+    });
+    return articles;
+}
+
+const updateArticle = async(id, data) => {
+    const validationData = articleSchema.parse(data)
+    const article = await prisma.article.update({
+        where:{id},
+        validationData
+    })
+    return article;
+}
+
+const deleteArticle = async(id) => {
+    const article = await prisma.article.delete({
+        where:{id}
+    })
+    return article;
+}
+const publishArticle = async(id) => {
+    const validationData = articleSchema.parse(data)
+    const article = await prisma.article.update({
+        where:{id},
+        validationData:{status:"PUBLISHED"}
+    })
+    return article;
+}
+const archiveArticle = async(id) => {
+    const validationData = articleSchema.parse(data)
+    
+    const article = await prisma.article.update({
+        where:{id},
+        validationData:{status:"ARCHIVED"}
+    })
+    return article;
+}
+
+export {
+    createArticle,
+    getArticles, 
+    getArticleById, 
+    getArticlesByCategory, 
+    getArticlesByAuthor, 
+    updateArticle, 
+    deleteArticle, 
+    publishArticle, 
+    archiveArticle, 
+    getAllArticles
+};
+// export default createArticle;
