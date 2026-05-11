0698e02e97f51eab425de2e8c17754a5fc3b0007
Mourchid Assani <mourchidhssn@gmail.com>
2026-05-11 10:34:18 +0200

    mise en place des autres endpoints du crud articles

--- backEnd/src/modules/articles/articleController.js
+++ backEnd/src/modules/articles/articleController.js
@@ -1,3 +1,4 @@
+import { tr } from "zod/v4/locales";
 import createArticle from "./articleService.js";
 import getArticles from "./articleService.js";
 import getArticleById from "./articleService.js";
@@ -18,4 +19,103 @@ const createArticleController = async (req, res, next) => {
     }
 }
 
-export default createArticleController;
+const getArticlesController = async (req, res, next) => {
+    try {
+        const articles = await getArticles();
+        res.json(articles);
+    } catch (error) {
+        next(error);
+    }
+}
+const getArticleByIdController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const article = await getArticleById(id);
+        res.json(article);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getArticlesByCategoryController = async (req, res, next) => {
+    try{
+        const { categoryId } = req.params;
+        const articles = await getArticlesByCategory(categoryId);
+        res.json(articles);
+    }catch(error){
+        next(error);
+    }
+}
+
+const getAllArticlesController = async (req, res, next ) => {
+    try {
+        const articles = await getAllArticles();
+        res.json(articles);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getArticlesByAuthorController = async(req, res, next) =>{
+    try{
+        const {authorId} = req.params;
+        const articles = await getArticlesByAuthor(authorId);
+        res.json(articles);
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+const updateArticleController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const article = await updateArticle(id, req.body);
+        res.json(article);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const deleteArticleController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const article = await deleteArticle(id);
+        res.json(article);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const publishArticleController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const article = await publishArticle(id);
+        res.json(article);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const archiveArticleController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const article = await archiveArticle(id);
+        res.json(article);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default {
+    getArticlesController,
+    getArticleByIdController,
+    getArticlesByCategoryController,
+    getAllArticlesController,
+    getArticlesByAuthorController,
+    updateArticleController,
+    deleteArticleController,
+    publishArticleController,
+    archiveArticleController,
+    createArticleController
+};
